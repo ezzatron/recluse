b68fe3000bb573d3d42a1a8cadf2aaa68c50f621
Erin Millard <ezzatron@gmail.com>
2019-04-30 23:07:35 +1000

    Add tests for acquireAsyncIterator()

--- src/iterator.js
+++ src/iterator.js
@@ -6,6 +6,8 @@ module.exports = {
  * Return an async iterator for the supplied async iterable.
  */
 function acquireAsyncIterator (asyncIterable) {
+  if (!asyncIterable) throw new Error('Supplied value is not an async iterable')
+
   const iteratorFactory = asyncIterable[Symbol.asyncIterator]
 
   if (typeof iteratorFactory !== 'function') throw new Error('Supplied value is not an async iterable')
