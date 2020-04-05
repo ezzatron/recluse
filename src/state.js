906f99dca024df510414c7393767f31c01d04cc5
Erin Millard <ezzatron@gmail.com>
2020-04-05 12:43:50 +1000

    Reinstate state controller

---
+++ src/state.js
@@ -0,0 +1,49 @@
+module.exports = {
+  createStateController,
+}
+
+function createStateController (copy, read) {
+  let isReadFlag = false
+  let isUpdatedFlag = false
+  let state = null
+
+  async function getState () {
+    if (!isReadFlag) {
+      state = await read()
+      isReadFlag = true
+    }
+
+    return state
+  }
+
+  function isUpdated () {
+    return isUpdatedFlag
+  }
+
+  async function readState () {
+    return copy(await getState())
+  }
+
+  async function updateState (produce) {
+    if (typeof produce !== 'function') {
+      state = copy(produce)
+      isReadFlag = true
+      isUpdatedFlag = true
+
+      return
+    }
+
+    const draft = await readState()
+    const result = await produce(draft)
+
+    state = copy(typeof result === 'undefined' ? draft : result)
+    isUpdatedFlag = true
+  }
+
+  return {
+    getState,
+    isUpdated,
+    readState,
+    updateState,
+  }
+}
